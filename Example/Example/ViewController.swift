420743bf26d26c85a39fa2b88803c0eb96a91354
cgoldsby <cgoldsby@gmail.com>
2017-03-08 16:39:34 -0500

    Updating Example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,17 +7,14 @@
 //
 
 import UIKit
-import ExpandingDescriptionView
+import TvOSMoreButton
+import TvOSTextViewer
 
 
 final class ViewController: UIViewController {
     
-    @IBOutlet private weak var leftExpandingDescriptionView: ExpandingDescriptionView!
-    
-    @IBOutlet private weak var middleExpandingDescriptionView: ExpandingDescriptionView!
+    @IBOutlet private weak var tvOSMoreButton: TvOSMoreButton!
 
-    @IBOutlet private weak var rightExpandingDescriptionView: ExpandingDescriptionView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
@@ -26,8 +23,11 @@ final class ViewController: UIViewController {
     // MARK: - Private
     
     private func setUpUI() {
-        leftExpandingDescriptionView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque a libero quis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam sem sit amet turpis gravida molestie a quis mauris. Sed metus nunc, imperdiet id vestibulum vulputate, ultricies quis erat. Quisque dictum, leo vitae pulvinar convallis, lorem lacus congue arcu, a mattis enim nisi eu diam. Phasellus vehicula iaculis maximus. Integer et ex felis. Curabitur nibh ex, commodo eget libero vitae, sodales pretium tellus. Maecenas at imperdiet lorem. Phasellus sed laoreet arcu. Mauris auctor lacus libero, vel blandit ex dapibus non. Ut at lectus nec magna tempus vehicula id at odio. Quisque cursus dui a efficitur fringilla. Ut vel lectus quis odio dignissim scelerisque. Pellentesque varius urna auctor quam faucibus commodo."
-        middleExpandingDescriptionView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque a libero quis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam sem sit amet turpis gravida molestie a quis mauris. Sed metus nunc, imperdiet id vestibulum vulputate, ultricies quis erat. Quisque dictum, leo vitae pulvinar convallis, lorem lacus congue arcu, a mattis enim nisi eu diam. Phasellus vehicula iaculis maximus. Integer et ex felis. Curabitur nibh ex, commodo eget libero vitae, sodales pretium tellus. Maecenas at imperdiet lorem. Phasellus sed laoreet arcu. Mauris auctor lacus libero, vel blandit ex dapibus non. Ut at lectus nec magna tempus vehicula id at odio. Quisque cursus dui a efficitur fringilla. Ut vel lectus quis odio dignissim scelerisque. Pellentesque varius urna auctor quam faucibus commodo.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque a libero quis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam sem sit amet turpis gravida molestie a quis mauris. Sed metus nunc, imperdiet id vestibulum vulputate, ultricies quis erat. Quisque dictum, leo vitae pulvinar convallis, lorem lacus congue arcu, a mattis enim nisi eu diam. Phasellus vehicula iaculis maximus. Integer et ex felis. Curabitur nibh ex, commodo eget libero vitae, sodales pretium tellus. Maecenas at imperdiet lorem. Phasellus sed laoreet arcu. Mauris auctor lacus libero, vel blandit ex dapibus non. Ut at lectus nec magna tempus vehicula id at odio. Quisque cursus dui a efficitur fringilla. Ut vel lectus quis odio dignissim scelerisque. Pellentesque varius urna auctor quam faucibus commodo.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque a libero quis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam sem sit amet turpis gravida molestie a quis mauris. Sed metus nunc, imperdiet id vestibulum vulputate, ultricies quis erat. Quisque dictum, leo vitae pulvinar convallis, lorem lacus congue arcu, a mattis enim nisi eu diam. Phasellus vehicula iaculis maximus. Integer et ex felis. Curabitur nibh ex, commodo eget libero vitae, sodales pretium tellus. Maecenas at imperdiet lorem. Phasellus sed laoreet arcu. Mauris auctor lacus libero, vel blandit ex dapibus non. Ut at lectus nec magna tempus vehicula id at odio. Quisque cursus dui a efficitur fringilla. Ut vel lectus quis odio dignissim scelerisque. Pellentesque varius urna auctor quam faucibus commodo.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque a libero quis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam sem sit amet turpis gravida molestie a quis mauris. Sed metus nunc, imperdiet id vestibulum vulputate, ultricies quis erat. Quisque dictum, leo vitae pulvinar convallis, lorem lacus congue arcu, a mattis enim nisi eu diam. Phasellus vehicula iaculis maximus. Integer et ex felis. Curabitur nibh ex, commodo eget libero vitae, sodales pretium tellus. Maecenas at imperdiet lorem. Phasellus sed laoreet arcu. Mauris auctor lacus libero, vel blandit ex dapibus non. Ut at lectus nec magna tempus vehicula id at odio. Quisque cursus dui a efficitur fringilla. Ut vel lectus quis odio dignissim scelerisque. Pellentesque varius urna auctor quam faucibus commodo."
-        rightExpandingDescriptionView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque a libero quis auctor."
+        tvOSMoreButton.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque a libero quis auctor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquam sem sit amet turpis gravida molestie a quis mauris. Sed metus nunc, imperdiet id vestibulum vulputate, ultricies quis erat. Quisque dictum, leo vitae pulvinar convallis, lorem lacus congue arcu, a mattis enim nisi eu diam. Phasellus vehicula iaculis maximus. Integer et ex felis. Curabitur nibh ex, commodo eget libero vitae, sodales pretium tellus. Maecenas at imperdiet lorem. Phasellus sed laoreet arcu. Mauris auctor lacus libero, vel blandit ex dapibus non. Ut at lectus nec magna tempus vehicula id at odio. Quisque cursus dui a efficitur fringilla. Ut vel lectus quis odio dignissim scelerisque. Pellentesque varius urna auctor quam faucibus commodo."
+        tvOSMoreButton.buttonWasPressed = moreButtonWasPressed
+    }
+    
+    private func moreButtonWasPressed(text: String?) -> Void {
+        
     }
 }
