773de4f1c3279cf5476c8d391aaa460ff62cad6e
cgoldsby <cgoldsby@gmail.com>
2018-10-14 08:26:02 -0400

    Fix line length swift violation

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -29,7 +29,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    // swiftlint:line_length
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         return true
     }
 }
