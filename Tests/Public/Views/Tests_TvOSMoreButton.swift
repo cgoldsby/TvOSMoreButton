155e7dbebb1da658c7eaa45f6776baa83f911116
cgoldsby <cgoldsby@gmail.com>
2022-04-18 11:49:15 -0400

    Fix warning when using Xcode 13.2.1

--- Tests/Public/Views/Tests_TvOSMoreButton.swift
+++ Tests/Public/Views/Tests_TvOSMoreButton.swift
@@ -37,7 +37,7 @@ final class Tests_TvOSMoreButton: XCTestCase {
         localizedStringBundle = .testBundle()
         let frame = CGRect(x: 0, y: 0, width: 500, height: 100)
         sut = TvOSMoreButton(frame: frame)
-//        record = true
+//        isRecording = true
     }
 
     override func tearDown() {
